9deffee7363b4d980a0fd746c887ba4c18e6eb0c
kaining <xkn1994@gmail.cin>
2021-04-07 14:24:35 +0200

    thrid commit

---
+++ ios-project/views/UrlImageView.swift
@@ -0,0 +1,20 @@
+//
+//  UrlImageView.swift
+//  ios-project
+//
+//  Created by Kaining Xin on 06/04/2021.
+//
+
+import SwiftUI
+
+struct UrlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UrlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        UrlImageView()
+    }
+}
